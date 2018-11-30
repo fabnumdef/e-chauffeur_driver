5b3be20828f4725207e7756877624fad6797e04a
Gaël <gael+git@demette.fr>
2018-11-30 15:45:37 +0100

    Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="columns">
+    <nuxt class="column" />
+  </div>
+</template>
+<script>
+export default {
+  components: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import "~assets/css/head";
+
+  .is-menu {
+    padding-right: 0;
+    background: $blue-dark;
+    margin-bottom: -$size-small;
+  }
+  .columns {
+    height: 100%;
+  }
+  .is-map {
+    margin-bottom: -$size-small * 2;
+  }
+</style>
