d232c23723509d8fe36e766028f48de3ebbf0fed
Gaël <gael+git@demette.fr>
2019-01-25 10:22:03 +0100

    Swiping cards from left to right and right to left

--- pages/_campus/index.vue
+++ pages/_campus/index.vue
@@ -1,12 +1,49 @@
 <template>
   <section>
-    <div v-swiper:ridesSwipe="{}">
+    <h1 class="title container">
+      Courses
+    </h1>
+    <div
+      v-if="rides.length < 1"
+      class="no-rides container"
+    >
+      En attente de courses, vous n'avez aucune course à effectuer pour l'instant.
+    </div>
+    <div
+      v-else
+      v-swiper:ridesSwipe="{}"
+    >
       <div class="swiper-wrapper">
-        <div class="swiper-pagination swiper-pagination-bullets"></div>
+        <div class="swiper-pagination swiper-pagination-bullets" />
         <div
           v-for="ride in rides"
-          :key="ride.id" class="swiper-slide"
+          :key="ride.id"
+          class="swiper-slide"
         >
+          <div class="notification is-primary">
+            {{ ride.departure.label }}
+            <ride-status :status="ride.status" />
+            <status-change :status="ride.status" />
+          </div>
+          <div class="box">
+            <a
+              v-if="ride.phone"
+              :href="`tel:${ride.phone}`"
+            >
+              <fa-icon :icon="['fas', 'phone']" />
+            </a>
+            <ul>
+              <li>Nombre de passagers : <strong>{{ ride.passengersCount }}</strong></li>
+              <li>Heure de prise en charge</li>
+            </ul>
+            <button
+              type="button"
+              class="button is-danger is-outlined"
+              @click="changeStatus(ride, 'cancel')"
+            >
+              Annuler la course
+            </button>
+          </div>
           {{ ride.id }}
           {{ ride.status }}
           <button
@@ -24,13 +61,22 @@
         </div>
       </div>
     </div>
+    <rides-to-accept :campus="campus" />
   </section>
 </template>
 <script>
 import { mapGetters } from 'vuex';
 import { actions } from '~/api/status';
+import rideStatus from '~/components/ride-status-badge.vue';
+import statusChange from '~/components/ride-status-change.vue';
+import ridesToAccept from '~/components/rides-to-accept.vue';
 
 export default {
+  components: {
+    rideStatus,
+    statusChange,
+    ridesToAccept,
+  },
   async asyncData({
     params, store,
   }) {
@@ -40,7 +86,7 @@ export default {
   computed: {
     actions: () => actions,
     ...mapGetters({
-      rides: 'rides/rides',
+      rides: 'rides/ridesToDo',
     }),
   },
   methods: {
@@ -50,3 +96,10 @@ export default {
   },
 };
 </script>
+<style scoped lang="scss">
+  @import "~assets/css/head";
+  .no-rides {
+    padding: 15px;
+    font-size: $size-4;
+  }
+</style>
