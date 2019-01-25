a4e9e664108992293bac60ff2b9e159bfeea5dbc
Gaël <gael+git@demette.fr>
2019-01-25 10:19:44 +0100

    Create a modal where we will show all rides to accept/decline

---
+++ components/rides-to-accept.vue
@@ -0,0 +1,94 @@
+<template>
+  <div
+    v-if="rides.length"
+    class="modal is-active"
+  >
+    <div class="modal-background" />
+    <div class="modal-content">
+      <div
+        v-for="ride in rides"
+        :key="ride.id"
+        class="box"
+      >
+        <h2 class="subtitle">
+          Course #{{ ride.id }}
+        </h2>
+        <ec-field
+          label="Départ"
+          :icon-left="['fas', 'map-marker-alt']"
+        >
+          <input
+            v-model="ride.departure.label"
+            disabled
+            class="input"
+          >
+        </ec-field>
+        <ec-field
+          label="Arrivée"
+          :icon-left="['fas', 'flag-checkered']"
+        >
+          <input
+            v-model="ride.arrival.label"
+            disabled
+            class="input"
+          >
+        </ec-field>
+
+        <ul>
+          <li>Nombre de passagers : <strong>{{ ride.passengersCount }}</strong></li>
+          <li>Heure de prise en charge : <strong>{{ ride.start|format_time('24_simple') }}</strong></li>
+          <li v-if="ride.car">
+            <strong v-if="ride.car.model">
+              {{ ride.car.model.label }}
+            </strong> / {{ ride.car.id }}
+          </li>
+        </ul>
+        <status-change
+          class="status-change"
+          :status="ride.status"
+          fullwidth
+          @change="updateRide(ride, $event)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+import statusChange from '~/components/ride-status-change.vue';
+import ecField from '~/components/form/field.vue';
+
+export default {
+  components: {
+    ecField,
+    statusChange,
+  },
+  props: {
+    campus: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    ...mapGetters({
+      rides: 'rides/ridesToAccept',
+    }),
+  },
+  methods: {
+    updateRide(ride, transition) {
+      return this.$api.rides(this.campus, this.$auth.user.id, 'id').mutateRide(ride.id, transition);
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+  @import "~assets/css/head";
+  @import "~bulma/sass/components/modal.sass";
+
+  .input[disabled] {
+    color: $text;
+    background: transparent;
+    border-color: $text;
+    font-size: 20px;
+  }
+</style>
