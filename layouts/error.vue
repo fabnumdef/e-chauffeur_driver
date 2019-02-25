accefbce36ab421ce12ab20495a5b5aa7d41db57
Gaël <gael+git@demette.fr>
2019-02-25 21:12:55 +0100

    Add error template

---
+++ layouts/error.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      Une erreur s'est produite
+    </h1>
+    <div class="box">
+      <p>
+        Erreur : {{ error.message }}
+      </p>
+      <nuxt-link
+        class="button"
+        to="/"
+      >
+        Retour à l'accueil
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  layout: 'default',
+};
+</script>
+
+<style scoped lang="scss">
+  @import "~assets/css/head";
+
+  .is-menu {
+    padding-right: 0;
+    background: $blue-dark;
+    margin-bottom: -$size-small;
+    min-height: 100vh;
+  }
+  .column.content-col {
+    padding: $size-medium !important;
+  }
+  .columns {
+    height: 100%;
+  }
+  .is-map {
+    margin-bottom: -$size-small * 2;
+  }
+</style>
