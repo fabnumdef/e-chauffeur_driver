f2aabb057c840185a1f44f798272d6cf201fc665
Gaël <gael+git@demette.fr>
2019-02-26 22:03:21 +0100

    Force french locale

--- plugins/luxon.js
+++ plugins/luxon.js
@@ -4,13 +4,13 @@ import { DateTime } from 'luxon';
 export default () => {
   Vue.filter('format_time', (value, format = 'simple') => {
     if (typeof value === 'string') {
-      return DateTime.fromISO(value).toLocaleString(DateTime[`time_${format}`.toUpperCase()]);
+      return DateTime.fromISO(value).setLocale('fr').toLocaleString(DateTime[`time_${format}`.toUpperCase()]);
     }
     return value;
   });
   Vue.filter('format_date', (value, format = 'full') => {
     if (value === 'now') {
-      return DateTime.local().toLocaleString(DateTime[`date_${format}`.toUpperCase()]);
+      return DateTime.local().setLocale('fr').toLocaleString(DateTime[`date_${format}`.toUpperCase()]);
     }
     return value;
   });
