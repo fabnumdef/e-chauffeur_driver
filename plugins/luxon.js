3c883f8050818a662662b12ea3a5ee20bcf7f201
Gaël <gael+git@demette.fr>
2019-02-26 22:22:45 +0100

    Force locale in luxon config

--- plugins/luxon.js
+++ plugins/luxon.js
@@ -1,16 +1,18 @@
 import Vue from 'vue';
-import { DateTime } from 'luxon';
+import { DateTime, Settings } from 'luxon';
+
+Settings.defaultLocale = 'fr';
 
 export default () => {
   Vue.filter('format_time', (value, format = 'simple') => {
     if (typeof value === 'string') {
-      return DateTime.fromISO(value).setLocale('fr').toLocaleString(DateTime[`time_${format}`.toUpperCase()]);
+      return DateTime.fromISO(value).toLocaleString(DateTime[`time_${format}`.toUpperCase()]);
     }
     return value;
   });
   Vue.filter('format_date', (value, format = 'full') => {
     if (value === 'now') {
-      return DateTime.local().setLocale('fr').toLocaleString(DateTime[`date_${format}`.toUpperCase()]);
+      return DateTime.local().toLocaleString(DateTime[`date_${format}`.toUpperCase()]);
     }
     return value;
   });
