66efb47183e95b7dc9c0bbf94cc236057662cac1
Gaël <gael+git@demette.fr>
2019-03-06 14:26:56 +0100

    Add system to join room

--- plugins/socket.js
+++ plugins/socket.js
@@ -2,11 +2,15 @@ import Vue from 'vue';
 import VueSocketio from 'vue-socket.io-extended';
 import io from 'socket.io-client';
 
+const authHeaderKey = '_token.local';
 
 export default function ({ env, store, app }, inject) {
   const ioInstance = io(env.apiUrl, { autoConnect: false });
   Vue.use(VueSocketio, ioInstance, { store });
   inject('io', ioInstance);
+  ioInstance.on('connect', () => {
+    ioInstance.emit('roomJoinDriver', app.$auth.$storage.getUniversal(authHeaderKey));
+  });
   const autoConnect = (isLogged) => {
     if (isLogged) {
       ioInstance.open();
