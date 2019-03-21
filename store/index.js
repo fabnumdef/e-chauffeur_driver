bf32c8784d54b7d52473334ce79684b78d27c2ec
Rodrive <robin.lebert@gmail.com>
2019-03-21 14:59:53 +0100

    Refactor take any service worker available

--- store/index.js
+++ store/index.js
@@ -1,23 +1,4 @@
-import { DateTime } from 'luxon';
-
-async function pushNotification(ride) {
-  if (!window.$sw || !ride || ride.status !== 'validated') {
-    return;
-  }
-  let { permission } = Notification;
-  if (permission !== 'granted' || permission !== 'denied') {
-    permission = await Notification.requestPermission();
-  }
-  if (permission === 'denied') {
-    return;
-  }
-  const message = `Nouvelle course à ${DateTime.fromISO(ride.start).setLocale('fr').toFormat('HH\'h\'mm')}`;
-  const body = `De ${ride.departure.label} à ${ride.arrival.label}`;
-  await window.$sw.showNotification(message, { body });
-  const [notif] = await window.$sw.getNotifications();
-  setTimeout(() => { notif.close(); }, 15000);
-}
-
+import pushNotification from '../plugins/push-notification';
 
 export const state = () => ({});
 
