1da087335e0539f638d1fe595a5e09fcd995c208
Gaël Demette <gael.demette+git@qonfucius.team>
2020-02-08 22:59:07 +0100

    Hotfix listening nullable

--- plugins/socket.js
+++ plugins/socket.js
@@ -2,21 +2,23 @@ import { AUTH_HEADER_KEY } from '@fabnumdef/e-chauffeur_lib-vue/plugins/socket';
 
 export default function ({ app, store }) {
   const ioInstance = app.$io;
-  ioInstance.on('connect', () => {
-    ioInstance.emit('roomJoinDriver', app.$auth.$storage.getUniversal(AUTH_HEADER_KEY));
-    store.dispatch('reconnecting', false);
-  });
-  ioInstance.on('reconnecting', () => {
-    store.dispatch('reconnecting', true);
-  });
+  if (ioInstance) {
+    ioInstance.on('connect', () => {
+      ioInstance.emit('roomJoinDriver', app.$auth.$storage.getUniversal(AUTH_HEADER_KEY));
+      store.dispatch('reconnecting', false);
+    });
+    ioInstance.on('reconnecting', () => {
+      store.dispatch('reconnecting', true);
+    });
 
-  const autoConnect = (isLogged) => {
-    if (isLogged) {
-      ioInstance.open();
-    } else {
-      ioInstance.close();
-    }
-  };
-  autoConnect(app.$auth.loggedIn);
-  app.$auth.$storage.watchState('loggedIn', autoConnect);
+    const autoConnect = (isLogged) => {
+      if (isLogged) {
+        ioInstance.open();
+      } else {
+        ioInstance.close();
+      }
+    };
+    autoConnect(app.$auth.loggedIn);
+    app.$auth.$storage.watchState('loggedIn', autoConnect);
+  }
 }
